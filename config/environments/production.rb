266e1e7d28bb01214dea323e90c4c034dbba592c
David Cloutier <algritz@videotron.ca>
2014-12-24 11:23:22 -0500

    updated binaries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,9 @@ LivreRecettesV3::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+  
+  config.log_level = :info
+  
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
