b29656a836f255382a552607cdd498df96c9311c
dcloutier <david.cloutier@oracle.com>
2017-10-13 15:30:49 -0400

    adding assets

--- app/controllers/recettes_controller.rb
+++ app/controllers/recettes_controller.rb
@@ -1,7 +1,7 @@
 class RecettesController < ApplicationController
-
-  before_filter :do_authentication, only: [:edit, :update, :destroy]
+  before_filter :do_authentication, only: %i[edit update destroy]
   before_filter :signed_in?, only: [:new]
+
   def home
     @recettes = Recette.search(params[:search])
     @pos = 0
@@ -14,17 +14,11 @@ class RecettesController < ApplicationController
   # GET /recettes
   # GET /recettes.json
   def index
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @pos = 0
-    if params != nil then
-      if params[:sort] == "creation" then
-        @recettes = Recette.all.order("id desc")
-      else
-        @recettes = Recette.all.order("titre asc")
-      end
-    else
-      @recettes = Recette.all.order("titre asc")
-    end
+    puts '################'
+    puts params
+    @recettes = Recette.all.order('id desc').limit(15)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -35,7 +29,7 @@ class RecettesController < ApplicationController
   # GET /recettes/1
   # GET /recettes/1.json
   def show
-    @current_page = "recettes"
+    @current_page = 'recettes'
 
     @recette = Recette.find(params[:id])
     @categories = Category.all
@@ -52,7 +46,7 @@ class RecettesController < ApplicationController
   # GET /recettes/new
   # GET /recettes/new.json
   def new
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @recette = Recette.new
     @categories = Category.all
     respond_to do |format|
@@ -63,25 +57,27 @@ class RecettesController < ApplicationController
 
   # GET /recettes/1/edit
   def edit
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @recette = Recette.find(params[:id])
   end
 
   # POST /recettes
   # POST /recettes.json
   def create
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @recette = Recette.new(recette_params)
     @recette.user_id = current_user[:id]
-    if params[:comment] != nil then
-      @comment = @recette.comments.new(:content => params[:comment][:content], :user_id => current_user[:id], :recette_id => @recette[:id])
+    unless params[:comment].nil?
+      @comment = @recette.comments.new(content: params[:comment][:content],
+                                       user_id: current_user[:id],
+                                       recette_id: @recette[:id])
     end
     respond_to do |format|
       if @recette.save
         format.html { redirect_to @recette, notice: 'Recette was successfully created.' }
         format.json { render json: @recette, status: :created, location: @recette }
       else
-        format.html { render action: "new" }
+        format.html { render action: 'new' }
         format.json { render json: @recette.errors, status: :unprocessable_entity }
       end
     end
@@ -90,9 +86,9 @@ class RecettesController < ApplicationController
   # PUT /recettes/1
   # PUT /recettes/1.json
   def update
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @recette = Recette.find(params[:id])
-    #params[:recette][:category_ids] ||= []
+    # params[:recette][:category_ids] ||= []
 
     @recette.update_attributes!(recette_params)
     redirect_to @recette
@@ -101,7 +97,7 @@ class RecettesController < ApplicationController
   # DELETE /recettes/1
   # DELETE /recettes/1.json
   def destroy
-    @current_page = "recettes"
+    @current_page = 'recettes'
     @recette = Recette.find(params[:id])
     @recette.destroy
 
@@ -113,12 +109,37 @@ class RecettesController < ApplicationController
 
   private
 
-  # Using a private method to encapsulate the permissible parameters is just a good pattern
-  # since you'll be able to reuse the same permit list between create and update. Also, you
-  # can specialize this method with per-user checking of permissible attributes.
+  # Using a private method to encapsulate the permissible parameters is just a good pattern since you'll be able to
+  # reuse the same permit list between create and update. Also, you can specialize this method with per-user
+  # checking of permissible attributes.
   def recette_params
-    params.require(:recette).permit( {:category_ids => []}, :user_id, :ingredient, :instruction, :portion, :preparation, :titre, :vin, :source, :note, :marinage, :cuisson, :accompagnement, :photo, :photos_attributes => [:signature, :created_at, :tags, :bytes, :type, :url, :secure_url, :etag, :public_id, :version, :width, :height, :format, :resource_type])
+    params.require(:recette).permit({ category_ids: [] },
+                                    :user_id,
+                                    :ingredient,
+                                    :instruction,
+                                    :portion,
+                                    :preparation,
+                                    :titre,
+                                    :vin,
+                                    :source,
+                                    :note,
+                                    :marinage,
+                                    :cuisson,
+                                    :accompagnement,
+                                    :photo,
+                                    photos_attributes: %i[signature
+                                                          created_at
+                                                          tags
+                                                          bytes
+                                                          type
+                                                          url
+                                                          secure_url
+                                                          etag
+                                                          public_id
+                                                          version
+                                                          width
+                                                          height
+                                                          format
+                                                          resource_type])
   end
-
 end
-
--- app/helpers/recettes_helper.rb
+++ app/helpers/recettes_helper.rb
@@ -1,31 +1,13 @@
+# helper module is accessible from the Recettes views
 module RecettesHelper
   def fill_blanks(count)
-
     count -= 1
-    the_string = ""
-    while count % 4 != 0 do
-      the_string += "<td></td>"
-      if count % 4 == 0 then
-      break
-      end
+    the_string = ''
+    while count % 4 != 0
+      the_string += '<td></td>'
+      break if count % 4.zero?
       count += 1
     end
-    return the_string
-
+    the_string
   end
-
-  def generate_sort_link
-    if params != nil then
-      if params[:sort] == "creation" then
-        link_to('Trier par titre', recettes_path) + " | Trier par ordre d'ajout"
-      else
-        raw("Trier par titre | " + link_to("Trier par ordre d'ajout", recettes_path+"?sort=creation"))
-      end
-    else
-      raw("Trier par titre | " + link_to("Trier par ordre d'ajout", recettes_path+"?sort=creation"))
-    end
-  end
-  
- 
-
 end
